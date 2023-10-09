9607426a001d7e74a2c15aba9f0e9d15d61d9c44
ambrose-kibet <ambrosekibet36@gmail.com>
2023-10-09 22:10:35 +0300

    add validation

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Patch, Delete, Body } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { SignUpUserDto } from './dtos/signup-user.dto';
+import { LoginUserDto } from './dtos/login-user.dto';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private authService: UsersService) {}
+  @Post()
+  signup(@Body() body: SignUpUserDto) {}
+  @Post()
+  login(@Body() body: LoginUserDto) {}
+}
