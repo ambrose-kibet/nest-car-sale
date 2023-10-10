b8040cb9977bddbcc3107e0e997bf2792177aafe
ambrose-kibet <ambrosekibet36@gmail.com>
2023-10-10 20:30:05 +0300

    add crud methods

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,13 +1,41 @@
-import { Controller, Get, Post, Patch, Delete, Body } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Patch,
+  Delete,
+  Body,
+  Param,
+  Query,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
 import { SignUpUserDto } from './dtos/signup-user.dto';
 import { LoginUserDto } from './dtos/login-user.dto';
+import { UpdateUserDto } from './dtos/update-user.dto';
+import { UpdateUserPasswordDto } from './dtos/update-password.dto';
 
-@Controller('users')
+@Controller('auth')
 export class UsersController {
   constructor(private authService: UsersService) {}
-  @Post()
-  signup(@Body() body: SignUpUserDto) {}
-  @Post()
+  @Post('/signup')
+  signup(@Body() body: SignUpUserDto) {
+    return this.authService.registerUser(
+      body.firstName,
+      body.lastName,
+      body.email,
+      body.password,
+    );
+  }
+  @Post('/login')
   login(@Body() body: LoginUserDto) {}
+  @Get('/:id')
+  findUser(@Param() id: string) {}
+  @Get('')
+  findUserByEmail(@Query('email') email: string) {}
+  @Patch()
+  updateUser(@Body() body: UpdateUserDto) {}
+  @Patch('/update-password')
+  updateUserPassword(@Body() body: UpdateUserPasswordDto) {}
+  @Delete('/:id')
+  deleteUser(@Param() id: string) {}
 }
