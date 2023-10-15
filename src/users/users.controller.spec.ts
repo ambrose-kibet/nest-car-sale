8969d9dc46c8ffb7c1bb8d176acadb2837a85884
ambrose-kibet <ambrosekibet36@gmail.com>
2023-10-15 22:05:41 +0300

    add user

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -1,18 +1,14 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
 
-describe('UsersController', () => {
-  let controller: UsersController;
+// describe('UsersController', () => {
+//   let controller: UsersController;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      controllers: [UsersController],
-    }).compile();
+//   beforeEach(async () => {
+//     const module: TestingModule = await Test.createTestingModule({
+//       controllers: [UsersController],
+//     }).compile();
 
-    controller = module.get<UsersController>(UsersController);
-  });
-
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
-  });
-});
+//     controller = module.get<UsersController>(UsersController);
+//   });
+// });
